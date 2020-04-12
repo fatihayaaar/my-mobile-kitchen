08a1796186c64a026d8867e98cdcc4f07e831842
Fatih Ayar <fatihayaaar@gmail.com>
2020-04-12 14:06:45 +0300

    Uygulama üst başlık yazısı düzenlendi
    closed #4

--- app/src/main/java/com/fayarretype/mymobilekitchen/activities/FoodsActivity.java
+++ app/src/main/java/com/fayarretype/mymobilekitchen/activities/FoodsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -15,16 +16,23 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class FoodsActivity extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    private androidx.appcompat.widget.Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foods);
 
-        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        loadToolbar(R.string.FoodsOptionLayoutName);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private void loadToolbar(@StringRes int resId) {
+        toolbar.setTitle(resId);
+        setSupportActionBar(toolbar);
     }
 
     private boolean loadFragment(Fragment fragment) {
@@ -44,10 +52,12 @@ public class FoodsActivity extends AppCompatActivity
 
         switch (menuItem.getItemId()) {
             case R.id.nav_view_foods:
+                loadToolbar(R.string.FoodsOptionLayoutName);
                 fragment = FragmentContainer.getInstance(this)
                         .getFragment(FragmentName.VIEW_FOODS_FRAGMENT);
                 break;
             case R.id.nav_add_food:
+                loadToolbar(R.string.tabFoodsAdd);
                 fragment = FragmentContainer.getInstance(this)
                         .getFragment(FragmentName.ADD_FOOD_FRAGMENT);
                 break;
